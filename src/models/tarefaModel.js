39f27e06b256022a87328d987eb9416fa1dccaa2
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-02-27 09:48:00 -0300

    feat: create tasks model and CRUD

---
+++ src/models/tarefaModel.js
@@ -0,0 +1,45 @@
+class TarefaModel {
+  constructor() {
+    this.tarefas = [
+        { id: 1, descricao: "Estudar Node.js", finalizada: false }
+    ];
+  }
+  getAll = () => this.tarefas;
+
+  // Cração de Nova Tarefa
+  create = (descricao) => {
+    const novaTarefa = {
+      id: this.tarefas.length + 1,
+      descricao,
+      finalizada: false,
+    };
+
+    this.tarefas.push(novaTarefa);
+    return novaTarefa;
+  };
+
+  // Atualização de Tarefa
+  update = (id, descricao, finalizada) => {
+    const tarefa = this.tarefas.finf((t) => t.id === Number(id));
+
+    if (tarefa) {
+        tarefa.concluida =
+            concluida !== undefined ? concluida : tarefa.concluida;
+        return tarefa;
+    }
+    return null;
+  };
+
+  // Remoção de tarefa
+  delerte = (id) => {
+    const index = this.tarefas.findIndex((t) => t.id === Number(id));
+
+    if (index !== -1) {
+        this.tarefas.splice(index, 1);
+        return true;
+    }
+    return false;
+  };
+}
+
+export default new TarefaModel;
